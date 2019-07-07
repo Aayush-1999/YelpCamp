8b42d3ba596afb1048747fefdb50441a2aed6e32
Aayush-1999 <aayushaggarwal2007@gmail.com>
2019-07-07 16:50:24 +0530

    modify app.js

--- app.js
+++ app.js
@@ -18,15 +18,6 @@ var campgroundRoute = require("./routes/campgrounds"),
     commentRoute    = require("./routes/comments"),
     indexRoute      = require("./routes/index");
 
-// const MongoClient = require("mongodb").MongoClient;
-// const uri = "mongodb+srv://aayush-agarwal:<A2%40agarwal>@cluster0-xtzpv.mongodb.net/test?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, { useNewUrlParser: true });
-// client.connect(err => {
-//   const collection = client.db("test").collection("devices");
-//   // perform actions on the collection object
-//   client.close();
-// });
-
 // mongoose.connect("mongodb://127.0.0.1/camp_data",{useNewUrlParser:true});
 mongoose.connect("mongodb+srv://aayush-agarwal:A2%40agarwal@cluster0-xtzpv.mongodb.net/test?retryWrites=true&w=majority",{useNewUrlParser:true});
 mongoose.set('useFindAndModify', false);
